a58f69e85807d485d7cd4fec671d70c306265fda
MuhammadUmairMoosani <muhammadumairmoosani@gmail.com>
2017-10-11 01:22:33 +0500

    SignIn And SignUp with firebase

---
+++ signinfirebase/src/config/fbconfig.js
@@ -0,0 +1,11 @@
+import * as firebase from 'firebase';
+ // Initialize Firebase
+ var config = {
+    apiKey: "AIzaSyAwNgAj-gY6cN6N1inXrN-N-tDm5V_t0AQ",
+    authDomain: "signin-signup-lap-project.firebaseapp.com",
+    databaseURL: "https://signin-signup-lap-project.firebaseio.com",
+    projectId: "signin-signup-lap-project",
+    storageBucket: "",
+    messagingSenderId: "694580551316"
+  };
+  firebase.initializeApp(config);
